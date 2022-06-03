08a9e4fb990dec8dea7f968703a323d47c303d4d
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-06-03 08:29:55 +0300

    auth added

---
+++ packages/auth/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import SignUp from './components/Signup';
+import SignIn from './components/Signin';
+
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'au',
+});
+const App = ({ history }) => {
+	return (
+		<StylesProvider generateClassName={generateClassName}>
+			<Router history={history}>
+				<Switch>
+					<Route  path={'/auth/signup'} component={SignUp} />
+					<Route  path="/auth/signin" component={SignIn} />
+				</Switch>
+			</Router>
+		</StylesProvider>
+	);
+};
+
+export default App;
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -6,6 +6,9 @@ const packages = require('../package.json');
 
 const devConfig = {
 	mode: 'development',
+	output: {
+		publicPath: 'http://localhost:8081/'
+	},
 	devServer: {
 		port: 8081,
 		historyApiFallback: {
